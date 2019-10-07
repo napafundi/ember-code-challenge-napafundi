9fa4775469c0115c911ad8d76109eac4b4f61179
Nick Pafundi <napafundi@gmail.com>
2019-10-07 17:45:42 -0400

    working filter searchbox that filters by full name, project and company

--- app/controllers/customers/index.js
+++ app/controllers/customers/index.js
@@ -1,15 +1,30 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
-import { sort } from '@ember/object/computed';
+import { sort} from '@ember/object/computed';
 
 export default Controller.extend({
   sortProperty: 'fullName',
   sortPropertyDesc: 'fullName:desc',
   sortPropertyBudgetDesc: 'budget:desc',
+  sortPropertyBudget: 'budget',
 
   customersSortProps: computed('sortProperty', function() {
     return [this.sortProperty];
   }),
 
-  sortedCustomers: sort('model', 'customersSortProps')
+  sortedCustomers: sort('model', 'customersSortProps'),
+  queryParams: ['search'],
+  search: '',
+  filteredCustomers: computed('sortedCustomers', 'search', function() {
+    var search = this.get('search').toLowerCase();
+    if (!search) {
+      return this.sortedCustomers;
+    } else {
+      return this.sortedCustomers.filter(function(item) {
+        return item.get('fullName').toLowerCase().indexOf(search) !== -1 ||
+               item.get('company').toLowerCase().indexOf(search) !== -1 ||
+               item.get('project').toLowerCase().indexOf(search) !== -1;
+      })
+    }
+  }).observes('searchTerm'),
 });
