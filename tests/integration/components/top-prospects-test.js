5b463b9fca7a7420c6a6409fde4cd6b72057d248
Nick Pafundi <napafundi@gmail.com>
2019-10-08 14:07:41 -0400

    fixed topProspects test to actually do something

--- tests/integration/components/top-prospects-test.js
+++ tests/integration/components/top-prospects-test.js
@@ -5,4 +5,21 @@ import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | top-prospects', function(hooks) {
   setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    this.set('model', [{
+      firstName: 'test-first',
+      lastName: 'test-last',
+      emailAddress: 'test@email.com',
+      phoneNumber: '(555) 555-5555',
+      company: 'test inc.',
+      project: 'test project',
+      budget: 500,
+    }]);
+  });
+
+  test('it renders', async function(assert) {
+    await render(hbs`<TopProspects @model={{this.model}} />`);
+    assert.ok(this.element.textContent);
+  })
 });
