31f413f8a3abc61e2f27b2af74517047f6a45907
Nick Pafundi <napafundi@gmail.com>
2019-10-08 12:52:11 -0400

    added collapsible section of glance-view that displays more model properties upon opening

--- app/components/glance-view.js
+++ app/components/glance-view.js
@@ -21,5 +21,33 @@ export default Component.extend({
         return this.model.filter(function(item) {
             return item.project.toLowerCase().indexOf('retire') === -1;
         }).length
-    })
+    }),
+
+    nonExistentBudgetCount: computed('model', 'model.@each.budget', function() {
+        return this.model.filter(function(item) {
+            return item.budget <= 0;
+        }).length;
+    }),
+
+    customersBelowAverageBudget: computed('model','model.@each.budget', 'averageBudget', function(){
+        const averageBudget = this.get('averageBudget');
+        return (this.model.filter(function(item) {
+            return (item.budget / 1000000).toFixed(3) < averageBudget;
+        }).length / this.model.length).toFixed(2);
+    }),
+
+    isCollapsed: true,
+
+    actions: {
+        toggleCollapsedInfo() {
+            const btn = document.getElementById('collapseButton');
+            if (btn.textContent.trim() === 'More') {
+                btn.textContent = 'Less';
+                this.set('isCollapsed', false);
+            } else {
+                btn.textContent = 'More';
+                this.set('isCollapsed', true);
+            }   
+        }
+    }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   redirect() {
-    return this.transitionTo('customers.index');
+    return this.transitionTo('dashboard');
   }
 });
